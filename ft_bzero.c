a5b42bb3f61c24911f2b2219199c3ae29b0f287b
Kendra Etter <kendraetter@MacBook-Pro-de-Kendra.local>
2024-10-03 15:33:23 +0200

    corrections + memset/memcpy

--- ft_bzero.c
+++ ft_bzero.c
@@ -6,13 +6,13 @@
 /*   By: keetter <keetter@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/03 13:43:50 by keetter           #+#    #+#             */
-/*   Updated: 2024/10/03 14:02:49 by keetter          ###   ########.fr       */
+/*   Updated: 2024/10/03 14:09:14 by keetter          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
-#include <string.h>
+//#include <stdio.h>
+//#include <string.h>
 
 void	ft_bzero(void *s, size_t n)
 {
@@ -26,7 +26,7 @@ void	ft_bzero(void *s, size_t n)
 	}
 }
 
-int main(void)
+/*int main(void)
 {
     char str[50] = "Hello, World!";  //s test
 
@@ -34,4 +34,4 @@ int main(void)
     ft_bzero(str, 6);  // Mettre à zéro les 6 premiers octets
     printf("Après ft_bzero: %s\n", str);  //après bzero
     return 0;
-}
+}*/
--- ft_memcpy.c
+++ ft_memcpy.c
@@ -1,26 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: keetter <keetter@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/01 17:09:17 by keetter           #+#    #+#             */
-/*   Updated: 2024/10/03 15:24:00 by keetter          ###   ########.fr       */
+/*   Created: 2024/10/03 14:18:34 by keetter           #+#    #+#             */
+/*   Updated: 2024/10/03 15:32:16 by keetter          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memset(void *b, int c, size_t len)
+void	ft_memcpy(void *dst, const void *src, size_t n)
 {
-	size_t i;
-	i = 0;
+	size_t = i;
 
-	while (i < len)
+	i = 0;
+	while (i < n)
 	{
-		((unsigned char *)b)[i] = (unsigned char) c;
+		((unsigned char *)dst)[i] == ((unsigned char *)src)[i];
 		i++;
 	}
-	return (b);
+	return (dst);
 }
